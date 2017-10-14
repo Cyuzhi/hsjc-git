cdfb1611a29785dc392f7143879579ed67ffec35
suncode <359802215@qq.com>
2017-10-14 21:40:33 +0800

    主干代码初始化

---
+++ trunk/hsjc_web/src/main/java/com/hsjc/hsjcweb/HsjcwebApplication.java
@@ -0,0 +1,12 @@
+package com.hsjc.hsjcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsjcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsjcwebApplication.class, args);
+	}
+}
