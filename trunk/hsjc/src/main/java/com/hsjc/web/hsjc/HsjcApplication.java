a41601355921971fe7bca3f356a350e0a3102bf6
suncode <359802215@qq.com>
2017-10-14 23:46:27 +0800

    t:

---
+++ trunk/hsjc/src/main/java/com/hsjc/web/hsjc/HsjcApplication.java
@@ -0,0 +1,12 @@
+package com.hsjc.web.hsjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsjcApplication.class, args);
+	}
+}
---
+++ trunk/hsjc/src/test/java/com/hsjc/web/hsjc/HsjcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsjc.web.hsjc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HsjcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
